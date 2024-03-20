6834da0d582b6df3eee029ba761751531e692956
GiThallysson <thallyssonva44@gmail.com>
2024-03-20 07:42:38 -0300

    Alterate por

--- Trabalho/conectando/api/src/main/java/com/prometecliente/competindo/server/Cliente.java
+++ Trabalho/conectando/api/src/main/java/com/prometecliente/competindo/server/Cliente.java
@@ -1,1 +1,18 @@
+class Cliente {
+    private String nome;
+
+    public Cliente() {}
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
 
